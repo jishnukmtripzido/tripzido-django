614628ec6f8447e45d40a8e4cbf4c0a74e1f4be9
jishnukm9 <jishnukm2010@gmail.com>
2026-05-25 16:34:36 +0530

    fix:showing response if search result is empty

--- apps/core/responses.py
+++ apps/core/responses.py
@@ -4,27 +4,16 @@ def success_response(data=None, message="Success", status=200):
     return Response({
         "success": True,
         "message": message,
-        "data": data or {}
+        "data": data if data is not None else []
     }, status=status)
 
 def error_response(message="Error", errors=None, status=400):
     return Response({
         "success": False,
         "message": message,
-        "errors": errors or {}
+        "errors": errors if errors is not None else []
     }, status=status)
 
 
 
 
-#  return error_response(
-#             message="Validation failed",
-#             errors=serializer.errors,
-#             status=status.HTTP_400_BAD_REQUEST,
-#         )
-
-#   return success_response(
-#             data=serializer.data,
-#             message="Account group retrieved successfully",
-#             status=status.HTTP_200_OK,
-#         )
