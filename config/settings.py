c7635114d4bd2dd62efc64778189493c6fde7578
jishnukmtripzido <jishnu@tripzido.com>
2026-09-08 17:15:01 +0530

    modify opt

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,8 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "ydf)aba#jrii=tync3o=i0pj(d+wyo*2g0p&1*6d$lf8wi6un4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
